650777391fbb004e2aa188bee537a99fe1e4cf54
Beau Jepson <beau.jepson@jepsonconsulting.com>
2018-09-11 19:05:59 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,6 +1,5 @@
 # Add  code here!
 def prime?(num)
-  return false if num <= 1
-  Math.sqrt(num).to_i.downto(2).each {|i| return false if num % i == 0}
-  true
+  return if num <= 1
+  (2..Math.sqrt(num)).none? { |i| (num % i).zero? }
 end
